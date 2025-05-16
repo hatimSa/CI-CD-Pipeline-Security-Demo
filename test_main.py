2219b68b3ef861f306ba06a3122d124251d4782d
hatimSa <hatimsalhi03@gmail.com>
2025-05-16 23:47:03 +0100

    test good

--- test_main.py
+++ test_main.py
@@ -1,6 +1,5 @@
 from main import get_api_key
 import os
-#comment out the next line
 def test_get_api_key(monkeypatch):
     monkeypatch.setenv("API_KEY", "test123")
     assert get_api_key() == "test123"
